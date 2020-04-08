7045e111332f6e4dbcda0ce8a06f1a114054e493
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2020-04-08 08:48:44 +0200

    5-mapping-of-datetimes-does-not-work - add converter to modelmapper for conversion between OffsetDateTime <-> LocalDateTime (Timezone UTC), adjust json-conversion in unit-tests to correct date-format

--- src/main/java/eu/tib/oersi/WebConfig.java
+++ src/main/java/eu/tib/oersi/WebConfig.java
@@ -1,5 +1,8 @@
 package eu.tib.oersi;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,13 @@ public class WebConfig {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    final ModelMapper modelMapper = new ModelMapper();
+    modelMapper.addConverter(
+        ctx -> ctx.getSource() == null ? null : ctx.getSource().atOffset(ZoneOffset.UTC),
+        LocalDateTime.class, OffsetDateTime.class);
+    modelMapper.addConverter(
+        ctx -> ctx.getSource() == null ? null : ctx.getSource().toLocalDateTime(),
+        OffsetDateTime.class, LocalDateTime.class);
+    return modelMapper;
   }
 }
