e91c7ec437df679136a9c4844a9fa2d334f22749
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2020-12-22 11:57:34 +0100

    Removed unnecessary methods
    Ref https://gitlab.com/oersi/oersi-backend/-/issues/20

--- src/main/java/org/oersi/repository/LabelRepository.java
+++ src/main/java/org/oersi/repository/LabelRepository.java
@@ -3,7 +3,6 @@ package org.oersi.repository;
 import org.oersi.domain.Label;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -12,7 +11,5 @@ import java.util.Optional;
 public interface LabelRepository extends JpaRepository<Label, Long> {
 
   Optional<Label> findByLanguageCodeAndLabelKey(String languageCode, String labelKey);
-  List<Label> findByLanguageCode(String languageCode);
-  List<Label> findByLanguageCodeAndGroupId(String languageCode, String groupId);
 
 }
