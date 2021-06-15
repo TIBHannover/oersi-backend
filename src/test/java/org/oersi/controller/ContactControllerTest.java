3866d4ca940b244891181030a66349d525fc3cdc
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-06-15 08:19:31 +0200

    Adjusted unit test
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/59

--- src/test/java/org/oersi/controller/ContactControllerTest.java
+++ src/test/java/org/oersi/controller/ContactControllerTest.java
@@ -10,10 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,30 +30,8 @@ class ContactControllerTest {
 
   @Test
   void testPostContact() throws Exception {
-    mvc.perform(post(CONTACT_CONTROLLER_BASE_PATH).contentType(MediaType.APPLICATION_FORM_URLENCODED)
-      .content(buildUrlEncodedFormEntity("email", "testmail@test.org", "message", "testmessage", "subject", "testsubject")))
+    mvc.perform(post(CONTACT_CONTROLLER_BASE_PATH).contentType(MediaType.APPLICATION_JSON)
+      .content("{\"email\": \"testmail@test.org\", \"message\": \"testmessage\", \"subject\": \"testsubject\"}"))
       .andExpect(status().isOk());
-  }
-
-  private String buildUrlEncodedFormEntity(String... params) {
-    if( (params.length % 2) > 0 ) {
-      throw new IllegalArgumentException("Need to give an even number of parameters");
-    }
-    StringBuilder result = new StringBuilder();
-    for (int i=0; i<params.length; i+=2) {
-      if( i > 0 ) {
-        result.append('&');
-      }
-      try {
-        result.
-          append(URLEncoder.encode(params[i], StandardCharsets.UTF_8.name())).
-          append('=').
-          append(URLEncoder.encode(params[i+1], StandardCharsets.UTF_8.name()));
-      }
-      catch (UnsupportedEncodingException e) {
-        throw new RuntimeException(e);
-      }
-    }
-    return result.toString();
   }
 }
